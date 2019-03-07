3c3d199a80ba38432fe679562180a3c65b50c905
kyle <kylechenjie@gmail.com>
2019-03-07 14:14:56 +0800

    添加API.adoc，修改schema.sql

--- src/test/java/com/pass0word1/demo/springtest/SpringtestPetControllerTests.java
+++ src/test/java/com/pass0word1/demo/springtest/SpringtestPetControllerTests.java
@@ -2,27 +2,50 @@ package com.pass0word1.demo.springtest;
 
 import com.google.gson.Gson;
 import com.pass0word1.demo.springtest.domain.Pet;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.transaction.AfterTransaction;
+import org.springframework.test.context.transaction.BeforeTransaction;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.jdbc.JdbcTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseBody;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 
 
 @RunWith(SpringRunner.class)
 @WebAppConfiguration
 @SpringBootTest
+@Rollback()
 public class SpringtestPetControllerTests {
+  final static Logger logger= LoggerFactory.getLogger(SpringtestPetControllerTests.class);
+
+  @Rule
+  public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation();
 
   @Autowired
   private WebApplicationContext wac;
@@ -32,34 +55,57 @@ public class SpringtestPetControllerTests {
 
   private MockMvc mockMvc;
 
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
+
   @Before
   public void setup() {
-    this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac)
+        .apply(documentationConfiguration(this.restDocumentation)).build();
+  }
+
+  @BeforeTransaction
+  void beforeTransaction() {
+    // logic to be executed before a transaction is started
+    logger.info("beforeTransaction");
+  }
+
+  @AfterTransaction
+  void afterTransaction() {
+    // logic to be executed after a transaction has ended
+    logger.info("afterTransaction");
   }
 
   @Test
+  @Transactional
   public void testCreatePet() throws Exception {
+    int beforeRowCount = JdbcTestUtils.countRowsInTable(jdbcTemplate, "PET");
+    Assert.assertEquals(0, beforeRowCount);
     Pet pet = new Pet();
     pet.setName("cat");
     pet.setStatus("available");
     mockMvc.perform(
         post("/pet").contentType(MediaType.APPLICATION_JSON_UTF8).content(gson.toJson(pet)))
-        .andDo(print())
-        .andExpect(status().isCreated()).andExpect(jsonPath("$.id").isNumber());
+        .andExpect(status().isCreated()).andExpect(jsonPath("$.id").isNumber())
+        .andDo(document("addPet", requestFields(fieldWithPath("name").description("宠物名字"),
+            fieldWithPath("status").description("宠物状态"))));
+    int afterRowCount = JdbcTestUtils.countRowsInTable(jdbcTemplate, "PET");
+    Assert.assertEquals(1, afterRowCount);
   }
 
   @Test
   public void testGetPetById() throws Exception {
+
     testCreatePet();
     mockMvc.perform(
         get("/pet/{id}", 1)
             .accept(MediaType.APPLICATION_JSON_UTF8)
-    ).andDo(print()).andExpect(status().isOk())
+    ).andExpect(status().isOk())
         .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
         .andExpect(jsonPath("$.id").value(1))
         .andExpect(jsonPath("$.name").isString())
         .andExpect(jsonPath("$.status").value("available"))
-    ;
+        .andDo(print());
   }
 
   @Test
@@ -67,9 +113,8 @@ public class SpringtestPetControllerTests {
     mockMvc.perform(
         get("/pet/{id}", 2)
             .accept(MediaType.APPLICATION_JSON_UTF8)
-    )
-        .andDo(print())
-        .andExpect(status().isNotFound());
+    ).andExpect(status().isNotFound())
+        .andDo(print());
   }
 
   @Test
